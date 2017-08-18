637e4536727b47f0391d86706eb9b50622b8b80e
ctiller15 <christiller2015@gmail.com>
2017-08-18 13:36:36 -0400

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+// var {ObjectID} = require('mongoose');
 
 var {mongoose} = require('./db/mongoose')
 var {Todo} = require('./models/todo');
@@ -28,6 +29,42 @@ app.get('/todos', (req, res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+// GET /todos/1234324
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	// if (!ObjectID.isValid(id)){
+	// 	return res.status(404).send();
+	// }
+
+	if(!mongoose.Types.ObjectId.isValid(id)){
+		res.status(404).send({});
+		return console.log("Invalid ID");
+	}
+
+	Todo.findById(id).then((todo) => {
+		if(todo){
+			res.send({todo});
+			console.log("successful!")
+		} else if(!todo){
+			res.status(404).send({});
+			console.log("Todo not found")
+		}
+	}, (err) => {
+		res.status(400).send({});
+		console.log("Error");
+	});
+	// validate id using isValid
+		// if not valid, 404 - send back empty body
+
+	// findById
+		// success
+			// if todo - send it back
+			// if no todo - send back 404 with empty body
+		// error
+			// 400 - not valid - send back nothing
 });
 
 
