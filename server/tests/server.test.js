388f2381e7b95e41ff22eccf2871d76a3afd45ad
ctiller15 <christiller2015@gmail.com>
2017-08-18 15:16:04 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'First test todo'
 }, {
+	_id: new ObjectID(),
 	text: 'Second test todo'
 }];
 
@@ -75,4 +78,37 @@ describe('POST /todos', () => {
 
 
 	// request post send empty obj. expect 400 use same format. Database assumption length = 0.
+	describe('GET /todos/:id', () => {
+		it('should return todo doc', (done) => {
+			request(app)
+			.get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+		});
+
+		it('should return 404 if todo not found', (done) => {
+			request(app)
+			.get(`/todos/${new ObjectID().toHexString()}`)
+			.expect(404)
+			.end(done);
+			// Make a request using an objectID.
+			// Call its tohexstring method.
+			// It should be a valid id but not in the collection.
+			// only expectation is status code.
+			// make sure you get a 404 back.
+		});
+
+		it('should return 404 for non-object ids', (done) => {
+			request(app)
+			.get(`/todos/123`)
+			.expect(404)
+			.end(done);
+			// /todos/123
+			// should fail and trigger a 404.
+			// a 404 status code is expected.
+		});
+	});
 });
